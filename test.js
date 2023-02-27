1d4b7fa6428a6e0e77ee1d6964a652b702cdc3e1
Irgey <116591589+Irgey@users.noreply.github.com>
2023-02-27 13:41:54 +0100

    JS tests created

---
+++ test.js
@@ -0,0 +1,19 @@
+//Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total);
+// const diff = apples - grapes;
+// console.log(diff);
+
+// Заміни вираз перевизначення комбінованим оператором +=.
+
+// let students = 100;
+// students += 50;
+// console.log(students);
+
+// Розбери пріоритет операторів в інструкції привласнення значення змінної result.
+
+// const result = 108 + 223 - 2 * 3 ** 5;
+// console.log(result);
