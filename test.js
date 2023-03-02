0271f8452549ed7f9158005c3dd7e26e6371a44a
Alina1Alina <119854810+Alina1Alina@users.noreply.github.com>
2023-03-02 15:37:03 +0200

    BINGO

--- test.js
+++ test.js
@@ -53,3 +53,34 @@
 // }
 
 // console.log(sumCount([[1, 3], 2, 5]));
+
+
+// Напиши функцію для гри в Бінго. Гравцю даються рандомні числа,які він збирає(в масив).
+//Для цього треба записати число в змінну.
+//Якщо гравець отримав число,яке в нього є,то він його не може взяти другий раз і функція викликає сама себе знову.
+//Коли гравець збирає числа від 1 до 5,то він перемагає і функція повертає рядок 'БІНГО' , масив очищається і фунція більше себе не викликає.
+// Рандомні числа исла отримуються за допомогою допоміжної функції RandomNumber.
+
+// function randomNumber(){
+// return (Math.random() * (5 - 1) + 1).toFixed(0)
+// }
+
+// let playerDesk = [];
+
+
+
+// function bingoGame() {
+//     let number = randomNumber()
+//     if (playerDesk.includes(number)) { 
+//         console.log(`Таке число вже є! ${number}`); 
+//         bingoGame()
+//         return 
+//     } playerDesk.push(number)
+//     console.log(`Додаємо число ${number}`);
+//     if (playerDesk.length === 5) { 
+//         console.log(`БІНГО! `);
+//         playerDesk.splice(0, playerDesk.length)
+//         return
+//     } bingoGame()
+// }
+// bingoGame()
