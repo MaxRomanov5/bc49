ef06c74ef4d812e5b738a70d6003ad085d8a8d0b
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-07 15:06:46 +0100

    11

--- test.js
+++ test.js
@@ -4,26 +4,28 @@
 //Друга  стрілочна функція буде виконуватись,якщо сума чисел в масиві менша 0
 // та поветрає рядок 'Користувач винен банку <число>!'
 //
-const resolveFunction = (number) => {
-  return `Користувач не боржник, сума на його балансі ${number}!`;
-};
+// const resolveFunction = (number) => {
+//   return `Користувач не боржник, сума на його балансі ${number}!`;
+// };
 
-const rejectFunction = (number) => {
-  return `Користувач винен банку ${Math.abs(number)}!`;
-};
+// const rejectFunction = (number) => {
+//   return `Користувач винен банку ${Math.abs(number)}!`;
+// };
 
-function bankAdmin(resolve, reject, arr) {
-  let total = 0;
-  arr.forEach((number) => {
-    total += number;
-  });
+// function bankAdmin(resolve, reject, arr) {
+//   let total = 0;
+//   arr.forEach((number) => {
+//     total += number;
+//   });
 
-  if (total >= 0) {
-    return resolve(total);
-  }
+//   if (total >= 0) {
+//     return resolve(total);
+//   }
 
-  return reject(total);
-}
+//   return reject(total);
+// }
 
-console.log(bankAdmin(resolveFunction, rejectFunction, [100, 500, -700]));
-console.log(bankAdmin(resolveFunction, rejectFunction, [100, 500, -300, 600]));
+// console.log(bankAdmin(resolveFunction, rejectFunction, [100, 500, -700]));
+// console.log(bankAdmin(resolveFunction, rejectFunction, [100, 500, -300, 600]));
+
+
